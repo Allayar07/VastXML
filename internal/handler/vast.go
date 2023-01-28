f9fa21ffe3326d68e65de8beef9ae3890d942b54
Atajan Babakulyyev <83182099+atadzan@users.noreply.github.com>
2023-01-28 09:23:59 +0500

    Update vast.go

--- internal/handler/vast.go
+++ internal/handler/vast.go
@@ -8,12 +8,17 @@ import (
 
 func (h *Handler) generateVast(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
+	
+// 	change this to id := c.ParamsInt("id)
+	
 	if err != nil {
 		return ErrorMessage(c, http.StatusBadRequest, "invalid id param")
 	}
 	if err = h.service.Vast.GenerateVast(id); err != nil {
 		return ErrorMessage(c, http.StatusInternalServerError, err.Error())
 	}
-
 	return c.Status(200).JSON("OK")
+	
+// 	I think it will look pretty like this
+// 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"Result":"OK"})
 }
